b3b464493af0cb8771d081afb0a1f5795d1a8827
ammar903@gmail.com <ammar903@gmail.com>
2018-04-16 23:39:36 +0300

    finished buffer

--- blink.c
+++ blink.c
@@ -1,25 +1,30 @@
 #include <msp430g2253.h>
 #define B1 0x0002
+#define B2 0x0001
+
 
 static int flag = 0;
 static int mode = 0;
 static int control = 0;
+static int climate = 0;
 static int low = 12;
 static int medium = 21;
 static int high = 25;
-static int *current;
-
+static int *current = &low;
+static int newValue = 0;
+static int i=0;
 
 
 
 char numberStr[5];
-
 char PressCountB1 = 0;
+char PressCountB2 = 0;
 char Pressed = 0;
 char PressRelease = 0;
 char data;
 
 void UARTSendArray(char *TxArray,  char ArrayLength);
+int atoi(const char *str);
 char Int2DecStr(char *str, unsigned int value);
 void ConfigureTimer0A(void);
 void ConfigureTimer1A(void);
@@ -57,29 +62,6 @@ void main(void)
     _BIS_SR(CPUOFF + GIE);          // Enter LPM0 w/ interrupt
 }
 
-// Timer A0 interrupt service routine (for green LED pwm)
-#pragma vector=TIMER0_A0_VECTOR
-__interrupt void Timer_A (void)
-{
-    static int inc;
-    if(!flag){
-    }
-    else {
-        //if CCR1 is at 1 or at CCR0 minus 1, toggle the value of inc
-        if ((TA0CCR1 == (TA1CCR0 - 1)) || (TA0CCR1 == 1)) {
-            inc = !inc;
-        }
-        // if inc is 1 increment CCR1, else decrement CCR1
-        if (inc) {
-            TA0CCR1++;
-        }
-        else {
-            TA0CCR1--;
-        }
-        TA0CTL &= ~TAIFG;
-    }
-}
-
 // Timer A0 interrupt service routine (for blinking light)
 #pragma vector=TIMER1_A0_VECTOR
 __interrupt void Timer1_A (void)
@@ -87,14 +69,12 @@ __interrupt void Timer1_A (void)
     if(!flag){
         control++;
         if (control == 3){
-            UARTSendArray("Standby\n", 9);
             P1OUT ^= BIT0; // Toggle P1.0
             control = 0;
         }
 
     }
     else {
-        UARTSendArray("Active\n", 8);
         P1OUT &= ~BIT0;
     }
 }
@@ -123,6 +103,31 @@ __interrupt void PORT1_ISR(void)
     else {/* add code here to handle other PORT1 interrupts, if any */}
 }
 
+#pragma vector = NMI_VECTOR
+__interrupt void nmi_isr(void)
+{
+    if (IFG1 & NMIIFG) // Check if NMI interrupt was caused by nRST/NMI pin
+    {
+        IFG1 &= ~NMIIFG; // clear NMI interrupt flag
+        if (WDTCTL & WDTNMIES) // falling edge detected
+        {
+            Pressed |= B2; // Set Switch 2 Pressed flag
+            PressCountB2 = 0; // Reset Switch 2 long press count
+            WDTCTL = WDT_MDLY_32 | WDTNMI; // WDT 32ms delay + set RST/NMI pin to NMI
+            // Note: WDT_MDLY_32 = WDTPW | WDTTMSEL | WDTCNTCL // WDT password + Interval mode + clear count
+            // Note: this will also set the NMI interrupt to trigger on the rising edge
+
+        }
+        else // rising edge detected
+        {
+            Pressed &= ~B2; // Reset Switch 1 Pressed flag
+            PressRelease |= B2; // Set Press and Released flag
+            WDTCTL = WDT_MDLY_32 | WDTNMIES | WDTNMI; // WDT 32ms delay + falling edge + set RST/NMI pin to NMI
+        }
+    } // Note that NMIIE is now cleared; the wdt_isr will set NMIIE 32ms later
+    else {/* add code here to handle other kinds of NMI, if any */}
+}
+
 #pragma vector = WDT_VECTOR
 __interrupt void wdt_isr(void)
 {
@@ -137,6 +142,7 @@ __interrupt void wdt_isr(void)
                            P1DIR &= ~BIT6;                  //green LED (p1.6) off
                            P1SEL &= ~BIT6;
                            flag = ~flag;
+                           UARTSendArray("Standby\n\r", 9);
             }
 
             else if (++PressCountB1 == 16 ) // Long press duration 32*32ms = 1s
@@ -144,19 +150,19 @@ __interrupt void wdt_isr(void)
                 TA0CCR1 &=0;                                             //reset ta0ccr1
                 switch(mode){
                     case 0:
-                        UARTSendArray("low\n", 5);
+                        UARTSendArray("low\n\r", 5);
                         TA0CCR1 |= 1;
                         current = &low;
                         mode++;
                         break;
                     case 1:
-                        UARTSendArray("medium\n", 8);
+                        UARTSendArray("medium\n\r", 8);
                         TA0CCR1 |= 500;
                         current = &medium;
                         mode++;
                         break;
                     case 2:
-                        UARTSendArray("high\n", 6);
+                        UARTSendArray("high\n\r", 6);
                         TA0CCR1 |= 1499;
                         current = &high;
                         mode = 0;
@@ -168,9 +174,28 @@ __interrupt void wdt_isr(void)
             if (++PressCountB1 == 16){
                     flag = ~flag;
                     P1OUT &= ~BIT0;
+                    UARTSendArray("Active\n\r", 8);
            }
         }
    }
+
+    if (Pressed & B2) // Check if switch 2 is pressed
+      {
+          if (++PressCountB2 == 32 ) // Long press duration 32ms*32 = 1s
+          {
+              climate = ~climate;
+                      if(climate){
+                          UARTSendArray("heat:", 4);
+                          UARTSendArray("\n\r",2);
+                      }
+                      else{
+                          UARTSendArray("cool:", 4);
+                          UARTSendArray("\n\r",2);
+                      }
+          }
+      }
+    IFG1 &= ~NMIIFG; // Clear the NMI interrupt flag (in case it has been set by bouncing)
+    P1IFG &= ~BIT3; // Clear the button interrupt flag (in case it has been set by bouncing)
     IE1 |= NMIIE; // Re-enable the NMI interrupt to detect the next edge
     P1IE |= BIT3; // Re-enable interrupt for the button on P1.3
 }
@@ -194,24 +219,52 @@ void UARTSendArray(char *TxArray, char ArrayLength){
 #pragma vector=USCIAB0RX_VECTOR
 __interrupt void USCI0RX_ISR(void)
 {
+    static char buffer[3];
+
+    buffer[0] = buffer[1];
+    buffer[1] = buffer[2];
+    buffer[2] = UCA0RXBUF;
     data = UCA0RXBUF;
+
     UARTSendArray("Received command: ", 18);
     UARTSendArray(&data, 1);
-    UARTSendArray("\n", 2);
+    UARTSendArray("\n\r",2);
 
-    *current = atoi(&data);
+    if (buffer[2] == '\n' | buffer[2] == '\r' )
+    {
+        UARTSendArray(buffer, 3);
+        UARTSendArray("\n\r",2);
 
-    UARTSendArray("L:", 2);
-    UARTSendArray(numberStr,Int2DecStr(numberStr, low));
-    UARTSendArray("\n", 2);
+        newValue = atoi(buffer);
 
-    UARTSendArray("m:", 2);
-    UARTSendArray(numberStr,Int2DecStr(numberStr, medium));
-    UARTSendArray("\n", 2);
+        UARTSendArray(numberStr,Int2DecStr(numberStr, newValue));
+        UARTSendArray("\n\r",2);
 
-    UARTSendArray("h:", 2);
-    UARTSendArray(numberStr,Int2DecStr(numberStr, high));
-    UARTSendArray("\n", 2);
+        if (newValue >= 5 && newValue <= 30)
+        {
+            *current = newValue;
+        }
+        else
+        {
+            UARTSendArray("Value out of range",18 );
+            UARTSendArray("\n\r",2);
+        }
+
+        newValue = 0;
+        UARTSendArray("L:", 2);
+        UARTSendArray(numberStr,Int2DecStr(numberStr, low));
+        UARTSendArray("\n\r",2);
+
+        UARTSendArray("m:", 2);
+        UARTSendArray(numberStr,Int2DecStr(numberStr, medium));
+        UARTSendArray("\n\r",2);
+
+        UARTSendArray("h:", 2);
+        UARTSendArray(numberStr,Int2DecStr(numberStr, high));
+        UARTSendArray("\n\r",2);
+
+    }
+
 }
 
 static const unsigned int dec[] = {
@@ -256,14 +309,12 @@ void ConfigureTimer1A(void){
 }
 
 void ConfigureUART(void){
-#define RXD BIT1
-#define TXD BIT2
     /* Configure hardware UART */
-    P1SEL |= RXD + TXD ; // P1.1 = RXD, P1.2=TXD
-    P1SEL2 |= RXD + TXD ; // P1.1 = RXD, P1.2=TXD
+    P1SEL |= BIT1 + BIT2 ; // P1.1 = RXD, P1.2=TXD
+    P1SEL2 |= BIT1 + BIT2 ; // P1.1 = RXD, P1.2=TXD
     UCA0CTL1 |= UCSSEL_2; // Use SMCLK
-    UCA0BR0 = 104; // Set baud rate to 9600 with 1MHz clock (Data Sheet 15.3.13)
-    UCA0BR1 = 0; // Set baud rate to 9600 with 1MHz clock
+    UCA0BR0 = 0x68; // Set baud rate to 9600 with 1MHz clock (Data Sheet 15.3.13)
+    UCA0BR1 = 0x00; // Set baud rate to 9600 with 1MHz clock
     UCA0MCTL = UCBRS0; // Modulation UCBRSx = 1
     UCA0CTL1 &= ~UCSWRST; // Initialize USCI state machine
     IE2 |= UCA0RXIE; // Enable USCI_A0 RX interrupt
